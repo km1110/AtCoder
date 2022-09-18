3a57f24c63c99f8f3c2766bb698318627931ae1f
kohki miyama <kmx.111010@gmail.com>
2022-09-18 10:03:37 +0900

    [add] 269 A

---
+++ problem/269/a.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define rrep(i, a, b) for (ll i = a; i >= b; i--)
+#define fore(i, a) for (auto &i : a)
+#define all(x) (x).begin(), (x).end()
+#define sortall(v) sort(v.begen(), v.end())
+
+using namespace std;
+using ll = long long;
+
+int main()
+{
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+
+    cout << (a + b) * (c - d) << endl
+         << "Takahashi" << endl;
+}
